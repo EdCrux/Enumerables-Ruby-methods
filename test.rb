a22be6640ad3e3d965e5567c03dff94f0907ad7d
Eduardo <jecruxiz@gmail.com>
2020-02-20 09:16:17 -0600

    :bug: Fixing a but related to my_none?
    Checking bugs when you pass nil as value in the array

--- test.rb
+++ test.rb
@@ -22,61 +22,48 @@ def my_each
   self
 end
 
-proc { |x| x }
-my_each
-
-
-
-
-
-
-
-
-
-
-
 def my_all?(data = nil)
   return my_all?(data) if block_given? && !data.nil?
+  arr = to_a
+  return false if arr.length == 1 && arr[0] == nil
   if block_given?
-    my_each { |i| return false unless yield(i) }
+    my_each { |i|  false unless yield(i) }
     true
   elsif data.nil?
-    arr = to_a
     index = 0
     while index <= arr.length-1
       return false unless arr[index].class == arr[index + 1].class
       index += 1  
     end
   elsif data.is_a? Regexp
-    my_each { |i| return false unless i.to_s.match(data) }
+    my_each { |i|  false unless i.to_s.match(data) }
   elsif data.is_a? Class
-    my_each { |i| return false unless i.is_a? data }
+    my_each { |i|  false unless i.is_a? data }
   else
-    my_each { |i| return false unless i == data }
+    my_each { |i|  false unless i == data }
   end
   true
 end
 
 def my_none?(data = nil)
   return my_none?(data) if block_given? && !data.nil?
-
+  return true if self.empty?
   if block_given?
     my_all? { |item| yield(item) ? false : true }
   elsif data.is_a? Regexp
-    my_all? { |i| return false if i.to_s.match(data) }
+    my_all? { |item| item.to_s.match(data) ?  false : true }
   elsif data.is_a? Class
-    my_all? { |i| return false if i.is_a? data }
+    my_all? { |item| item.class == data ? false : true }
   elsif data.nil?
-    my_all?(data)
+     my_all?(data) ? false : true 
   end
 end
 
-p %w{ant bear cat}.my_all? { |word| word.length === 5 } #=> false
-p %w{ant bear cat}.my_none? { |word| word.length == 5 } #=> true
-p %w{ant bear cat}.my_none? { |word| word.length >= 4 } #=> false
-p %w{ant bear cat}.my_none?(/d/)                        #=> true
-p [1, 3.14, 42].my_none?(Float)                         #=> false
-p [].my_none?                                           #=> true
-p [nil].my_none?                                        #=> true
-p [nil, false].my_none?                                 #=> true
-p [nil, false, true].my_none?                           #=> false
+%w{ant bear cat}.none? { |word| word.length == 5 } #=> true
+%w{ant bear cat}.none? { |word| word.length >= 4 } #=> false
+%w{ant bear cat}.none?(/d/)                        #=> true
+[1, 3.14, 42].none?(Float)                         #=> false
+[].none?                                           #=> true
+[nil].none?                                        #=> true
+[nil, false].none?                                 #=> true
+p [nil, false, true].my_all?                           #=> false
